060b0d735c054d932a422f23b4c553a474fd9ccf
菜刀文 <halen917@foxmail.com>
2017-01-02 20:26:52 +0800

    init demo

---
+++ JitpackReleaseDemo/app/src/main/java/com/helen/jitpackreleasedemo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.helen.jitpackreleasedemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ JitpackReleaseDemo/testlib/src/main/java/com/helen/jitpackdemo/testlib/Util.java
@@ -0,0 +1,11 @@
+package com.helen.jitpackdemo.testlib;
+
+import android.util.Log;
+
+public class Util {
+
+    //要发布的lib 示例
+    public static void sayHi() {
+        Log.d("Util", "hello jitpack!");
+    }
+}
